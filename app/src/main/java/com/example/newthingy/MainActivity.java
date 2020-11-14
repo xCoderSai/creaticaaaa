ef2baf9d786ee09f470b9acc3cbd008142af1ed1
SandhyaS101 <sandhusk03@gmail.com>
2020-11-13 22:29:03 -0500

    sandhyaupdates

--- app/src/main/java/com/example/newthingy/MainActivity.java
+++ app/src/main/java/com/example/newthingy/MainActivity.java
@@ -1,56 +1,115 @@
 package com.example.newthingy;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import android.view.Menu;
-import android.view.MenuItem;
+public class MainActivity extends AppCompatActivity
+{
+    /* Define the UI elements */
+    private EditText eName;
+    private EditText ePassword;
+    private TextView eAttemptsInfo;
+    private Button eLogin;
+    private int counter = 5;
 
-public class MainActivity extends AppCompatActivity {
+    String userName = "";
+    String userPassword = "";
+
+    /* Class to hold credentials */
+    class Credentials
+    {
+        String name = "Admin";
+        String password = "123456";
+    }
+
+    boolean isValid = false;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        /* Bind the XML views to Java Code Elements */
+        eName = findViewById(R.id.etName);
+        ePassword = findViewById(R.id.etPassword);
+        eAttemptsInfo = findViewById(R.id.tvAttempts);
+        eLogin = findViewById(R.id.btnLogin);
+
+        /* Describe the logic when the login button is clicked */
+        eLogin.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View view)
+            {
+                /* Obtain user inputs */
+                userName = eName.getText().toString();
+                userPassword = ePassword.getText().toString();
+
+                /* Check if the user inputs are empty */
+                if(userName.isEmpty() || userPassword.isEmpty())
+                {
+                    /* Display a message toast to user to enter the details */
+                    Toast.makeText(MainActivity.this, "Please enter name and password!", Toast.LENGTH_LONG).show();
+
+                }
+                else
+                {
+                    /* Validate the user inputs */
+                    isValid = validate(userName, userPassword);
+
+                    /* Validate the user inputs */
+
+                    /* If not valid */
+                    if (!isValid)
+                    {
+                        /* Decrement the counter */
+                        counter--;
+                        /* Show the attempts remaining */
+                        eAttemptsInfo.setText("Attempts Remaining: " + String.valueOf(counter));
+
+                        /* Disable the login button if there are no attempts left */
+                        if (counter == 0)
+                        {
+                            eLogin.setEnabled(false);
+                            Toast.makeText(MainActivity.this, "You have used all your attempts try again later!", Toast.LENGTH_LONG).show();
+                        }
+                        /* Display error message */
+                        else
+                        {
+                            Toast.makeText(MainActivity.this, "Incorrect credentials, please try again!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                    /* If valid */
+                    else
+                    {
+                        /* Allow the user in to your app by going into the next activity */
+                        startActivity(new Intent(MainActivity.this, FirstFragment.class));
+                    }
+
+                }
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /* Validate the credentials */
+    private boolean validate(String userName, String userPassword)
+    {
+        /* Get the object of Credentials class */
+        Credentials credentials = new Credentials();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        /* Check the credentials */
+        if(userName.equals(credentials.name) && userPassword.equals(credentials.password))
+        {
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
+        return false;
     }
 }
